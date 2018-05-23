b6a18f78a682b1a49affccd7bac9faeb9d649d7b
Unknown <daniel.lorenzen@hotmail.com>
2018-05-23 13:30:15 +0200

    Ny camera metode
    Virker ikke

---
+++ SurvivalExam/SurvivalExam/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SurvivalExam
+{
+    public class Camera
+    {
+        public Matrix Transform { get; private set; }
+
+        public void Follow(SpriteRenderer target)
+        {
+            Transform = Matrix.CreateTranslation(-target.Position.X - (target.Rectangle.Width / 2), -target.Position.Y - (target.Rectangle.Height / 2),0);
+        }
+    }
+}
--- SurvivalExam/SurvivalExam/Player.cs
+++ SurvivalExam/SurvivalExam/Player.cs
@@ -109,5 +109,7 @@ namespace SurvivalExam
         {
 
         }
+
+        public Player(Texture2D)
     }
 }
